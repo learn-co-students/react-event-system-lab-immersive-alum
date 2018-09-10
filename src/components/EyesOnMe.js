b1b7f2b9654233ab4e904891c9a1f64de332a0fe
= <=>
2018-09-09 23:06:54 -0400

    tony-solution

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react'
+
+export default class EyesOnMe extends Component {
+
+    
+  render = () => (
+    <div>
+      <button
+        onFocus={() => console.log('Good!')}
+        onBlur={() => console.log('Hey! Eyes on me!')}
+      >
+        button
+      </button>
+    </div>
+  )
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
-// Code Keypad Component Here
+import React, { Component } from 'react'
+
+export default class Keypad extends Component {
+  handleKeyUp = () => 
+    console.log('Entering password...')
+  render = () => (
+    <div>
+      <input type="password" onKeyUp={this.handleKeyUp} />
+    </div>
+  )
+}
